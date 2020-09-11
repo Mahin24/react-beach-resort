f7c872d52cd3782d9d1a3c9bf10daa4102a52614
Mahin24 <mahin242424@gmail.com>
2020-09-12 02:01:48 +0600

    navbar banner complete

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,5 +1,15 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 export const Error = () => {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title="404" subtitle="not found">
+        <Link to="/" className="btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
